5875c8b2fa35bb3bb0ef4b9fffdac31402c898f6
mitchellnel <mitchellmclin@gmail.com>
2020-09-28 18:18:24 +0800

    Implemented addition of start and site_selection handlers

--- custom_news_bot.py
+++ custom_news_bot.py
@@ -8,20 +8,42 @@ from telegram.ext.dispatcher import run_async # rm
 import schedule # rm
 from selenium import webdriver # rm
 
-import functions.utility
-import functions.hltv
+import functions.utility as utility
+import functions.hltv as hltv
 import functions.formula1 as f1
-import functions.thespike as spike
+import functions.thespike as thespike
+import functions.mmoc as mmoc
 
 logging.basicConfig(format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level = logging.INFO)
+logger = logging.getLogger(__name__)
 
-token_file = open("BOT_TOKEN.txt", 'r')
-bot_token = token_file.readline()
-token_file.close()
+def main():
+    # get token from .txt file
+    token_file = open("BOT_TOKEN.txt", 'r')
+    bot_token = token_file.readline().rstrip()
+    token_file.close()
 
-updater = Updater(token = bot_token, use_context = True)
-dispatcher = updater.dispatcher
+    updater = Updater(token = bot_token, use_context = True)
+    dispatcher = updater.dispatcher
 
-# intialise commands using CommandHandlers
-start_handler = CommandHandler("start", utility.start)
+    # intialise and add "Handlers"
+    # start_handler to handle first-time start sequence
+    start_handler = CommandHandler("start", utility.start)
+    dispatcher.add_handler(start_handler)
+
+    # site_selection_handler to handle user input from site selection buttons
+    site_selection_handler = CallbackQueryHandler(utility.site_selection_button)
+    dispatcher.add_handler(site_selection_handler)
+
+    # start the bot
+    updater.start_polling()
+
+    logging.log(level=logging.INFO, msg = "Bot is live and ready to roll.")
+
+    # run the bot until the user presses ctrl-C or the process receives SIG-INT/TERM/ABRT
+    updater.idle()
+
+if __name__ == "__main__":
+    main()
+
